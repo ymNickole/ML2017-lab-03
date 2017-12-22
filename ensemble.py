37ae9bac57a82faa3ac053ea36fde850fd26c3de
nickole <510788094@qq.com>
2017-12-22 10:39:29 +0800

    update2

--- ensemble.py
+++ ensemble.py
@@ -1,4 +1,6 @@
 import pickle
+import numpy as np
+import os
 
 class AdaBoostClassifier:
     '''A simple AdaBoost Classifier.'''
@@ -10,7 +12,9 @@ class AdaBoostClassifier:
             weak_classifier: The class of weak classifier, which is recommend to be sklearn.tree.DecisionTreeClassifier.
             n_weakers_limit: The maximum number of weak classifier the model can use.
         '''
-        pass
+        self.weak_classifier = weak_classifier
+        self.n_weakers_limit = n_weakers_limit
+        self.alphas = np.zeros(self.n_weakers_limit)
 
     def is_good_enough(self):
         '''Optional'''
@@ -23,8 +27,33 @@ class AdaBoostClassifier:
             X: An ndarray indicating the samples to be trained, which shape should be (n_samples,n_features).
             y: An ndarray indicating the ground-truth labels correspond to X, which shape should be (n_samples,1).
         '''
-        pass
+        num = X.shape[0]    # 样本数量
+        w = np.ones(num)/num
+        if not os.path.exists('base_classifier'):
+            os.mkdir('base_classifier')
+        for i in range(self.n_weakers_limit):
+            print('training base classifier %d/%d' % (i+1,self.n_weakers_limit))
+            # Configure Decision tree
+            b_learner = self.weak_classifier(random_state=0, max_depth=2)
+            # Build Decision tree according training data
+            b_learner.fit(X, y, sample_weight=w)
+            # Save Model to a file in order to use it next time without build model step
+            self.save(b_learner,'base_classifier/base_classifier_%d.pkl' % i)
 
+            pre_y = b_learner.predict(X)
+
+            # 计算分类误差率
+            errorVector = np.zeros(num)
+            errorVector[pre_y != y] = 1
+            errorRate = np.dot(errorVector,w)
+            if errorRate > 0.5:
+                break
+            alpha = 0.5 * np.log(( 1 - errorRate ) / errorRate)
+            self.alphas[i] = alpha
+            w = w * np.exp( - alpha * y * pre_y )
+            w = w / w.sum()
+
+        print('training finish')
 
     def predict_scores(self, X):
         '''Calculate the weighted sum score of the whole base classifiers for given samples.
@@ -35,7 +64,14 @@ class AdaBoostClassifier:
         Returns:
             An one-dimension ndarray indicating the scores of differnt samples, which shape should be (n_samples,1).
         '''
-        pass
+        pre_ys = []
+        for i in range(self.n_weakers_limit):
+            b_learner = self.load('base_classifier/base_classifier_%d.pkl' % i)
+            pre_y = b_learner.predict(X)
+            pre_ys.append(pre_y)
+        pre_ys = np.array(pre_ys)
+        final_pre_y = np.dot(self.alphas, pre_ys)
+        return final_pre_y
 
     def predict(self, X, threshold=0):
         '''Predict the catagories for geven samples.
@@ -47,7 +83,11 @@ class AdaBoostClassifier:
         Returns:
             An ndarray consists of predicted labels, which shape should be (n_samples,1).
         '''
-        pass
+        print('predicting validation datasets...')
+        final_pre_y = self.predict_scores(X)
+        final_pre_y = np.array(list(map((lambda x: 1 if x >= threshold else -1), final_pre_y)))
+        print('predicted results:', final_pre_y)
+        return final_pre_y
 
     @staticmethod
     def save(model, filename):
--- train.py
+++ train.py
@@ -1,6 +1,64 @@
+from PIL import Image
+from feature import NPDFeature
+from ensemble import AdaBoostClassifier
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import classification_report
+import numpy as np
+import glob
+import os
+import random
+import pickle
+
+def loadDataSet():
+    if not os.path.exists('tmp.pkl'):
+        samples = []
+        # 遍历数据集目录
+        for img_name in sorted(glob.glob('datasets/original/face/*.jpg')):
+            # 读取正样本图像
+            print(img_name)
+            img = np.array(Image.open(img_name).resize((24, 24)).convert("L"))
+            # 提取NPD特征
+            features = NPDFeature(img).extract()
+            sample = np.r_[features, 1]  # 在正样本特征后面加一个Label为1
+            samples.append(sample)
+        for img_name in sorted(glob.glob('datasets/original/nonface/*.jpg')):
+            # 读取负样本图像
+            print(img_name)
+            img = np.array(Image.open(img_name).resize((24, 24)).convert("L"))
+            # 提取NPD特征
+            features = NPDFeature(img).extract()
+            sample = np.r_[features, -1]  # 在负样本特征后面加一个Label为-1
+            samples.append(sample)
+        # 数据集打乱
+        random.shuffle(samples)
+        dataset = np.array(samples)
+        with open('tmp.pkl', 'wb') as output:
+            pickle.dump(dataset, output, True)
+
+    with open('tmp.pkl', 'rb') as input:
+        dataset = pickle.load(input)
+        print(dataset.shape)
+    # 将数据集切分为训练集和验证集
+    X_train = dataset[:dataset.shape[0] * 3 // 4, :dataset.shape[1] - 1]
+    y_train = dataset[:dataset.shape[0] * 3 // 4, dataset.shape[1] - 1]
+    X_validation = dataset[dataset.shape[0] * 3 // 4:, :dataset.shape[1] - 1]
+    y_validation = dataset[dataset.shape[0] * 3 // 4:, dataset.shape[1] - 1]
+    return X_train, X_validation, y_train, y_validation
 
 
 if __name__ == "__main__":
-    # write your code here
-    pass
-
+    X_train, X_validation, y_train, y_validation = loadDataSet()
+    abc = AdaBoostClassifier(DecisionTreeClassifier, 20)
+    abc.fit(X_train, y_train)
+    final_pre_y = abc.predict(X_validation)
+    error = 0
+    for i in range(final_pre_y.shape[0]):
+        if final_pre_y[i] != y_validation[i]:
+            error = error + 1
+    accuracy = 1 - error / y_validation.shape[0]
+    print('accuracy: %f' % accuracy)
+    target_names = ['face', 'nonface']
+    report = classification_report(y_validation, final_pre_y, target_names=target_names)
+    print(report)
+    with open('report.txt', 'w') as f:
+        f.write(report)
